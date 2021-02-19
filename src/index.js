b68b4886385525da894e44f08cfecca8a0fd67c4
akansha <ranakansha321@gmail.com>
2021-02-19 16:04:35 +0530

    first commit git branch -M main git remote add origin https://github.com/ranakansha/react-learning.git git commit -m "first commit"

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+
+// render element in home page
+
+// ReactDOM.render(
+// <h1>Hello World</h1>,
+// document.getElementById('root')
+// );
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = 'hello word';
+// document.getElementById('root').appendChild(h1);
+
+// ======================render multiple html tag inside render method ================================
+// ReactDOM.render(
+//   <div>
+//   <h3>Index page</h3>
+//   <p>hello this is my page</p>
+//   </div>,
+// document.getElementById('root')
+// );
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <React.Fragment>
+  <h3>Index page</h3>
+  <p>hello this is my page</p>
+  </React.Fragment>,
+ 
+document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
