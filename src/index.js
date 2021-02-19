503e72ee6dec3581e61a5c432a8eadc6c4feb190
akansha <ranakansha321@gmail.com>
2021-02-19 17:34:51 +0530

    basic project

--- src/index.js
+++ src/index.js
@@ -2,30 +2,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
-// render element in home page
-
-// ReactDOM.render(
-// <h1>Hello World</h1>,
-// document.getElementById('root')
-// );
-
-// var h1 = document.createElement("h1");
-// h1.innerHTML = 'hello word';
-// document.getElementById('root').appendChild(h1);
-
-// ======================render multiple html tag inside render method ================================
-// ReactDOM.render(
-//   <div>
-//   <h3>Index page</h3>
-//   <p>hello this is my page</p>
-//   </div>,
-// document.getElementById('root')
-// );
 ReactDOM.render(
-  <React.Fragment>
-  <h3>Index page</h3>
-  <p>hello this is my page</p>
-  </React.Fragment>,
- 
-document.getElementById('root')
+  <>
+  <title>Netflix</title>
+  <h1>Unlimited movies, TV shows and more.</h1>
+  <p>Watch anywhere. Cancel anytime.</p>
+  <ol>
+    <li>Mirzapur</li>
+    <li>Scraed Game</li>
+    <li>Game of Thrones</li>
+    <li>Lords of 7 Rings</li>
+
+  </ol>
+  </>,
+  document.getElementById('root')
 );
